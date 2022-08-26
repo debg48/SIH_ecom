22d0ea8e693a16f86d34fa704497efc6cdb58e31
Debgandhar Ghosh <66957584+debg48@users.noreply.github.com>
2022-08-26 08:54:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request
 from flask_cors import CORS
 from recom import recommend
+import json
 
 app= Flask(__name__)
 CORS(app)
@@ -10,10 +11,11 @@ def index():
     try:
         q=request.args.get('q')
         data=recommend(q)
-        return {
+        
+        return json.dumps({
             'data':data,
             'success':True
-        }
+        },indent=4)
     except:
         return {
             'message':'Error',
